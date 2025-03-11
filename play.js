fa343784ed8c8db2e3ab0a10a8b04d1d0fc65d64
Alec Faustino <alecfaustino@outlook.com>
2025-03-10 22:07:30 -0600

    add function to handleUserInput and to setUpInput

--- play.js
+++ play.js
@@ -1,4 +1,22 @@
 const connect = require('./client');
 
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;  // create variable to hold the stdin object so we don't have to type process.stdin multiple times
+  stdin.setRawMode(true); // Raw Mode allows us to listen for individual keypresses instead of waiting for the user to press enter
+  stdin.setEncoding("utf8"); // utf8 encoding is set so that we can read the text data that is input
+  stdin.resume(); // resume stdin so the program can listen for input
+  setupInput.on("data", handleUserInput);
+  return stdin;   // return the stdin object so we can use it elsewhere in the program
+};
+
+const handleUserInput = (data) => {
+  if (data === "\u0003") {
+    process.exit();
+  }
+};
+
 console.log("Connecting ...");
 connect();
