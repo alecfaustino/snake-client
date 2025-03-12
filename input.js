780c3b855ed2f67105ca42201b2936ba5e62c659
Alec Faustino <alecfaustino@outlook.com>
2025-03-11 21:50:17 -0600

    add arrow keys to move and keys to send messages

--- input.js
+++ input.js
@@ -18,8 +18,17 @@ const handleUserInput = (data) => {
     'w': 'Move: up',
     'a': 'Move: left',
     's': 'Move: down',
-    'd': 'Move: right'
+    'd': 'Move: right',
+    '\u001b[A': 'Move: up',
+    '\u001b[B': 'Move: down',
+    '\u001b[D': 'Move: left',
+    '\u001b[C': 'Move: right',
+    '1': 'Say: mmm!',
+    '2': 'Say: you stole my lunch!',
+    '3': 'Say: good job!',
+    '4': 'Say: That is mine!'
   };
+
 
   if (keyMap[data]) {
     connection.write(keyMap[data]);
